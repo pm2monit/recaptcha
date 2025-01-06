a1c4d5967dbe32d1a988d798a8328efd5f96fcf2
Sosmed OnlyQ <sosmedonly1945@gmail.com>
2025-01-07 00:10:53 +0700

    menambahkan dan memperbaiki unit test

--- tests/RecaptchaTest.php
+++ tests/RecaptchaTest.php
@@ -5,11 +5,45 @@ use Recaptcha\Recaptcha;
 
 final class RecaptchaTest extends TestCase
 {
-    public function testVerifyCaptcha(): void
+    private $length = 10;
+    private ?Recaptcha $obj;
+
+    protected function setUp(): void
     {
-        $obj = new Recaptcha(20);
-        $captcha = $obj->getCaptcha('text');
-        $result = $obj->verifyCaptcha($captcha, 'text');
-        $this->assertIsBool($result);
+        $this->obj = new Recaptcha($this->length);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->obj = null;
+    }
+
+    public function testVerifyCaptcha(): void
+    {        
+        $captcha = $this->obj->getCaptcha('text');
+        $result = $this->obj->verifyCaptcha($captcha, 'text');
+
+        $this->assertTrue($result);
+    }
+    
+    public function testCaptchaTextLength(): void
+    {
+        $captcha = $this->obj->getCaptcha('text');
+
+        $this->assertEquals($this->length, strlen($captcha));
+    }
+
+    public function testCaptchaTextIsString(): void
+    {
+        $captcha = $this->obj->getCaptcha('text');
+
+        $this->assertIsString($captcha);
+    }
+
+    public function testCaptchaNumberIsString(): void
+    {
+        $captcha = $this->obj->getCaptcha('number');
+
+        $this->assertIsString($captcha);
     }
 }
