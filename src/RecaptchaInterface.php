8416cf0f0092f49865de8c4a40ee61d550fb4ac6
LMU DEV <development.web@lmu.co.id>
2025-01-09 10:36:18 +0700

    edit  captcha interface

--- src/RecaptchaInterface.php
+++ src/RecaptchaInterface.php
@@ -12,13 +12,13 @@ interface RecaptchaInterface
     /**
      * Set captcha text.
      *
-     * @param int $length
+     * @param int $length panjang captcha
      * @return string
      */
     public static function setCaptcha(int $length): void;
     
     /**
-     * Get captcha text or number.
+     * Get captcha set property text or number.
      *
      * @param CaptchaType $type Tipe captcha yang diinginkan (CaptchaType::TEXT atau CaptchaType::NUMBER)
      * @return string
@@ -26,17 +26,16 @@ interface RecaptchaInterface
     public static function getCaptcha(CaptchaType $type): string;
 
         /**
-     * Get captcha text or number.
+     * Generator captcha image.
      *
-     * @param string
-     * @return string
+     * @param string $captcha hasil dari get captcha
+     * @return void
      */
     public static function captchaImage(string $captcha): void;
     
     /**
      * Mencocokan hasil captcha
      * 
-     * string $inputText
      * @param string $inputan  inputan user
      * @return boolean
      */
